a47f1fbe2c2698b8c2be277e1204a9643b0c5b2f
jmoya99 <--global>
2019-01-26 16:46:44 -0500

    commit provicional

--- Entregable 1 POO/Estudiante.py
+++ Entregable 1 POO/Estudiante.py
@@ -32,7 +32,7 @@ class Estudiante(Persona):
     @staticmethod
     def eliminar(estudiante,list_estu,list_notas,list_matri):
         Matricula.eliminar_por_estudiante(list_matri,estudiante)
-        Nota.eliminar_por_estudiante(list_estu,estudiante)
+        Nota.eliminar_por_estudiante(list_notas,estudiante)
         return Persona.eliminar(list_estu,estudiante)
 
     @staticmethod

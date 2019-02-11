d5b97866e1f55fb7e89a4c8c552711db694b889d
jmoya99 <jmoya@unal.edu.co>
2019-02-11 18:58:11 -0500

    Correción clase mensajes

--- Entregable 1 POO/Mensajes.py
+++ Entregable 1 POO/Mensajes.py
@@ -3,60 +3,44 @@ class Mensajes:
            "ideEstu": "Identificación del estudiante: ", "ideMate": "Identificación de la materia: ", "menuMatt": "1. Buscar materia\n2. Generar reporte\n3. Salir\n",
            "numGrp": "Número del grupo: ", "ideNot": "Identificación de la nota: ", "reporte": "Reporte generado", "reportErr": "Error inesperado: Permiso denegado.",
            "nom": "Nombre", "cre": "Creditos", "gru": "Grupo", "reg": "Registrado", "mod": "Modificado",
-           "err": "Error", "eli": "Eliminado", "num": "Numero", "pro": "Profesor", "notFinal": "Nota final", "fech": "Fecha", "det": "Detalles",
-           "usu": "Usuario", "ide": "Identificación", "cor": "Correo", "con": "Contraseña", "Nonotas": "Materia sin notas", "Nogrup": "Materia sin grupos",
-           "not": "Nota", "por": "Porcentaje", "val": "Valor", "ing Usu": "Ingrese su identificación o correo electronico: ", 
-           "opnot": "1. Registrar\n2. Modificar\n3. Borrar\n4. Buscar\n5. Salir\n", "noenc": "No encontrado", "CorrE": "Correo enviado", "Noestnot": "Estudiante sin notas",
-           "ing Con": "Ingrese su contraseña: ", "bie": "Bienvenido", "o": "o", "enc": "Encontrad@", "Nomatric": "Grupo sin matricula",
+           "err": "Error", "eli": "Eliminado", "num": "Numero", "pro": "Profesor", "notFinal": "Nota final","fech": "Fecha","det":"Detalles",
+           "usu": "Usuario", "ide": "Identificación", "cor": "Correo", "con": "Contraseña", "Nonotas":"Materia sin notas","Nogrup":"Materia sin grupos",
+           "not": "Nota", "por": "Porcentaje", "val": "Valor", "ing Usu": "Ingrese su identificación o correo electronico: ","_":"__________________________",
+           "opnot": "1. Registrar\n2. Modificar\n3.Borrar\n4. Buscar\n5. Salir\n", "noenc": "No encontrado","CorrE":"Correo enviado","Noestnot":"Estudiante sin notas",
+           "ing Con": "Ingrese su contraseña: ", "bie": "Bienvenido", "o": "o", "enc": "Encontrad@", "Nomatric":"Grupo sin matricula",
            "opc1": "1. Iniciar sesión\n2. Cerrar aplicación\n3. Recuperar contraseña\n",
            "opcAdmin": "1. Estudiante\n2. Profesor\n3. Administrador\n4. Materia\n5. Grupo\n6. Cerrar sesion\n",
-           "opcProf": "1. Nota\n2. Grupo\n3. Materia\n4. Nuevo evento\n5. Mejores estudiantes\n6. Cerrar sesion\n", "nuSem": "Nuevo semestre ",
+           "opcProf": "1. Nota\n2. Grupo\n3. Materia\n4. Nuevo evento\n5. Cerrar sesion\n", "nuSem": "Nuevo semestre ",
            "CASE": "1. Registrar\n2. Consultar\n3. Modificar\n4. Eliminar\n5. Listar \n6. Volver\n", "busgrp": "1. Buscar grupo\n2. Salir\n",
            "CASEGru": "1. Registrar\n2. Consultar\n3. Modificar\n4. Eliminar\n5. Listar todo\n6. Listar por materia \n7. Mostrar mejor grupo por materia \n8. Volver\n",
            "CASEest": "1. Registrar\n2. Consultar\n3. Modificar\n4. Eliminar\n5. Listar \n6. Estudiantes que van perdiendo\n7. Volver\n",
-           "codi": "Codigo: ", "recu": "Recuperación de contraseña", "nuecon": "Nueva Contraseña: ", "menuEstu": "1. Nota\n2. Matrícula\n3. Grupo\n4. Materia\n5. Usuario\n6. Salir\n",
+           "codi": "Codigo: ", "recu": "Recuperación de contraseña", "nuecon": "Nueva Contraseña: ", "menuEstu": "1. Nota\n2. Matrícula\n3. Grupo\n4. Materia\n5. Salir\n",
            "registro": "registro", "modifico": "modifico", "borro": "borro", "cuerpo_borro": "se le borro la nota llamada ",
-           "cuerpo_borro2": " de la materia ", "cuerpo_resto": "su nota en ", "cuerpo_resto2": "con nombre de ",
+           "cuerpo_borrado2": " de la materia ", "cuerpo_resto": "su nota en ", "cuerpo_resto2": "con nombre de ",
            "cuerpo_resto3": " fue de ", "cuerpo_resto4": " y vale ", "cuerpo_resto5": " % de la nota final",
-<<<<<<< HEAD
-           "asunto": "Se le ", "asunto2": " una nota", "numNota": "Numero(Id) de la nota", "sem": "Semestre",
-           "emailSatisfactory": "Se ha enviado un correo al estudiante con la operacion realizada.",
-           "mejores_notas": "Esta es la lista de las 3 mejores notas de la asignatura en general",
-           1: "Primero. " , 2 : "Segundo. ", 3 : "Tercero. ", "cuerpo_best" : "con la nota de ", "cuerpo_best2" : ", lograda por "}
-=======
            "asunto": "se le ", "asunto2": " una nota", "numNota": "Numero(Id) de la nota", "sem": "Semestre",
-           "emailSatisfactory": "Se ha enviado un correo al estudiante con la operacion realizada.", "modi": "Modificar",
-           "opUsu": "1. Nombre\n2. Contraseña\n3. Volver\n"}
->>>>>>> Moya
+           "emailSatisfactory": "Se ha enviado un correo al estudiante con la operacion realizada."}
 
     ing = {"mat": "Subject", "idi": "1 for Spanish, 2 for English", "id": "Id", "idePro": "Teacher identification",
            "numGrp": "Group Number: ", "ideNot": "Score Identification: ", "reporte": "Report Generated", "reportErr": "Error unexpected: Permission Denied",
            "ideEstu": "Student identification: ", "ideMate": "Subject identification: ", "menuMatt": "1. Search Subject\n2. Generate report\n3. Exit\n",
            "nom": "Name", "cre": "Credits", "gru": "Group", "reg": "Inserted", "mod": "Modified",
-           "err": "Error", "eli": "Removed", "num": "Number", "pro": "Teacher", "notFinal": "Final note", "fech": "Date", "det": "Details",
-           "usu": "User", "ide": "Identification", "cor": "Email", "con": "Password", "Nonotas": "Subject without grades", "Nogrup": "Subject without groups",
-           "not": "Score", "por": "Percentage", "val": "Value", "ing Usu": "Enter your identification or email: ", 
-           "opnot": "1. Insert\n2. Modify\n3. Delete\n4. Search\n5. Exit\n", "noenc": "Not found", "CorrE": "Email sent", "Noestnot": "Student without grades",
-           "ing Con": "Enter your password: ", "bie": "Welcome", "o": "or", "enc": "Found", "Nomatric": "Group without enrollment",
+           "err": "Error", "eli": "Removed", "num": "Number", "pro": "Teacher", "notFinal": "Final note","fech": "Date", "det":"Details",
+           "usu": "User", "ide": "Identification", "cor": "Email", "con": "Password", "Nonotas":"Subject without grades","Nogrup":"Subject without groups",
+           "not": "Score", "por": "Percentage", "val": "Value", "ing Usu": "Enter your identification or email: ", "_":"__________________________",
+           "opnot": "1. Insert\n2. Modify\n3.Delete\n4. Search\n5. Exit\n", "noenc": "Not found", "CorrE":"Email sent","Noestnot": "Student without grades",
+           "ing Con": "Enter your password: ", "bie": "Welcome", "o": "or", "enc": "Found","Nomatric":"Group without enrollment",
            "opc1": "1. Log in\n2. Close application\n3. Recover password\n", "nuSem": "New semester",
            "opcAdmin": "1. Student\n2. Professor\n3. Administrator\n4. Subject\n5. Group\n6. Log Out\n",
-           "opcProf": "1. Score\n2. Group\n3. Subject\n4. New event\n5. Best students \n6. Log Out\n",
+           "opcProf": "1. Score\n2. Group\n3. Subject\n4. New event\n5. Log Out\n",
            "CASE": "1. Register\n2. Consult\n3. Modify\n4. Delete\n5. To list\n6. Go back\n", "busgrp": "1. Search group\n2. Exit\n",
            "CASEest": "1. Register\n2. Consult\n3. Modify\n4. Delete\n5. To list\n6 Students who are losing\n7 Go back\n",
            "CASEGru": "1. Register\n2. Consult\n3. Modify\n4. Delete\n5. List all\n6. List by subject\n7. Show best group by subject\n8. Go back\n",
-           "codi": "Code: ", "recu": "Password recovery", "nuecon": "New Password: ", "menuEstu": "1. Note\n2. Enrollment\n3. Group\n4. Subject\n5. User\n6. Exit\n",
+           "codi": "Code: ", "recu": "Password recovery", "nuecon": "New Password: ", "menuEstu": "1. Note\n2. Enrollment\n3. Group\n4. Subject\n5. Exit\n",
            "registro": "register", "modifico": "modify", "borro": "delete", "cuerpo_borro": "Was deleted your score called ",
            "cuerpo_borro2": " from the signature ", "cuerpo_resto": "Your note in ", "cuerpo_resto2": "with name ",
            "cuerpo_resto3": " was ", "cuerpo_resto4": " and has ", "cuerpo_resto5": " % from the final signature score",
-<<<<<<< HEAD
-           "asunto": "It's had been ", "asunto2": " a score", "numNota": "Number(Id) of the note", "sem": "Semester",
-           "emailSatisfactory": "We have send an email with the operation made",
-           "mejores_notas": "This is the list of the three first scores in the signature",
-           1: "First. " , 2 : "Second. ", 3 : "Third. ", "cuerpo_best" : "with the score of ", "cuerpo_best2" : ", reached by "}
-=======
            "asunto": "it's had been ", "asunto2": " a score", "numNota": "Number(Id) of the note", "sem": "Semester",
-           "emailSatisfactory": "We have send an email with the operation made", "modi": "Modify",
-           "opUsu": "1. Name\n2. Password\n3. Exit\n"}
->>>>>>> Moya
+           "emailSatisfactory": "We have send an email with the operation made"}
 
     mensa = {}
